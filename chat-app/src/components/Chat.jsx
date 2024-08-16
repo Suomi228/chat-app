85e8be72a9b38a76c1608cc4ff26fe58e1f31a2d
Suomi228 <bixep345@gmail.com>
2024-08-16 17:45:27 +0300

    chat messeges

--- chat-app/src/components/Chat.jsx
+++ chat-app/src/components/Chat.jsx
@@ -4,15 +4,15 @@ import { AttachFile, MoreVert, SearchOutlined } from "@material-ui/icons";
 import "./Chat.css";
 const names = ["Sam", "Angel", "Oliver", "Lola", "Cookie", "Annie"];
 const Chat = () => {
-    const [seed, setSeed] = useState("");
-    useEffect(() => {
-      const randomName = names[Math.floor(Math.random() * names.length)];
-      setSeed(randomName);
-    }, []);
+  const [seed, setSeed] = useState("");
+  useEffect(() => {
+    const randomName = names[Math.floor(Math.random() * names.length)];
+    setSeed(randomName);
+  }, []);
   return (
     <div className="chat">
       <div className="chat__header">
-        <Avatar src={`https://api.dicebear.com/9.x/dylan/svg?seed=${seed}`}/>
+        <Avatar src={`https://api.dicebear.com/9.x/dylan/svg?seed=${seed}`} />
         <div className="chat__headerInfo">
           <h3>Room Name</h3>
           <p>Last seen at...</p>
@@ -29,7 +29,23 @@ const Chat = () => {
           </IconButton>
         </div>
       </div>
-      <div className="chat__body"></div>
+      <div className="chat__body">
+        <p className="chat__message">
+          <span className="chat__name">Nabendu</span>
+          This is a message
+          <span className="chat__timestamp">{new Date().toUTCString()}</span>
+        </p>
+        <p className="chat__message chat__receiver">
+          <span className="chat__name">Parag</span>
+          This is a message back
+          <span className="chat__timestamp">{new Date().toUTCString()}</span>
+        </p>
+        <p className="chat__message">
+          <span className="chat__name">Nabendu</span>
+          This is a message again again
+          <span className="chat__timestamp">{new Date().toUTCString()}</span>
+        </p>
+      </div>
       <div className="chat__footer"></div>
     </div>
   );
