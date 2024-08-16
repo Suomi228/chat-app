9c9324591d5d8c1b6b8b3dbb622b6e89a153b964
Suomi228 <bixep345@gmail.com>
2024-08-16 16:21:14 +0300

    sidebarchat

---
+++ chat-app/src/components/SidebarChat.jsx
@@ -0,0 +1,23 @@
+import React, { useEffect, useState } from "react";
+import { Avatar } from "@material-ui/core";
+import "./SidebarChat.css";
+const names = ["Sam", "Angel", "Oliver", "Lola", "Cookie", "Annie"];
+const SidebarChat = () => {
+  const [seed, setSeed] = useState("");
+  useEffect(() => {
+    const randomName = names[Math.floor(Math.random() * names.length)];
+    setSeed(randomName);
+  }, []);
+  return (
+    <div className="sidebarChat">
+      <Avatar
+        src={`https://api.dicebear.com/9.x/dylan/svg?seed=${seed}`}
+      />
+      <div className="sidebarChat__info">
+        <h2>Room name</h2>
+        <p>Last message...</p>
+      </div>
+    </div>
+  );
+};
+export default SidebarChat;
