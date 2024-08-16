dbd7d33c3767150ad2af9b5ad17ec5e153ac831e
Suomi228 <bixep345@gmail.com>
2024-08-16 17:58:47 +0300

    chat footer

--- chat-app/src/components/Chat.jsx
+++ chat-app/src/components/Chat.jsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from "react";
 import { Avatar, IconButton } from "@material-ui/core";
-import { AttachFile, MoreVert, SearchOutlined } from "@material-ui/icons";
+import { AttachFile, MoreVert, SearchOutlined, InsertEmoticon } from "@material-ui/icons";
+import MicIcon from "@material-ui/icons/Mic";
 import "./Chat.css";
 const names = ["Sam", "Angel", "Oliver", "Lola", "Cookie", "Annie"];
 const Chat = () => {
@@ -46,7 +47,14 @@ const Chat = () => {
           <span className="chat__timestamp">{new Date().toUTCString()}</span>
         </p>
       </div>
-      <div className="chat__footer"></div>
+      <div className="chat__footer">
+        <InsertEmoticon />
+        <form>
+          <input placeholder="Type a message" type="text" />
+          <button type="submit">Send a message</button>
+        </form>
+        <MicIcon />
+      </div>
     </div>
   );
 };
